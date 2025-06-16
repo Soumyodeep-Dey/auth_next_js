df6c737aaa4a1d5a752266bb8afb412a83f9d1d0
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-06-16 20:37:41 +0530

    basic signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,18 @@
+export default function SignupPage() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <label>
+          Email:
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" required />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
