4c3910cacef8e311fe95f6a4e99989a9bb4a761c
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-06-16 21:02:17 +0530

    i think db connection will be okey

---
+++ src/db/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const mongoURL = process.env.mongo_url;
+
+export async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+        connection.on("connected", () => {
+            console.log("Connected to MongoDB");
+        });
+        connection.on("error", (err) => {
+            console.error("MongoDB connection error:", err);
+            process.exit();
+        });
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw error;
+    }
+}
