5d30d113d4af070de101ee243cfecb867ca5d09b
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-06-20 11:47:58 +0530

    debugging

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -1,18 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        await mongoose.connect(process.env.MONGO_URI!);
+        mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
-        connection.on("connected", () => {
-            console.log("Connected to MongoDB");
-        });
-        connection.on("error", (err) => {
-            console.error("MongoDB connection error:"+ err);
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
             process.exit();
-        });
+        })
+
     } catch (error) {
-        console.error("MongoDB connection error:", error);
-        throw error;
+        console.log('Something goes wrong!');
+        console.log(error);
+
     }
+
 }
