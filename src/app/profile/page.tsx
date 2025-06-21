0deba865c11fafd0a27e3cd469aadf3fb947fb2c
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-06-21 10:38:26 +0530

    logout is working properly

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,23 @@
 "use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import React from "react";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    // Implement your logout logic here
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.error("Logout failed:", error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-black py-12 px-4 sm:px-6 lg:px-8">
       <div className="card max-w-md w-full space-y-8">
@@ -35,6 +53,12 @@ export default function ProfilePage() {
               Change Password
             </button>
           </div>
+          <hr />
+          <button 
+          onClick={logout}
+          className="btn-danger w-full mt-4 hover:bg-red-700 transition-colors duration-200  focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 rounded-md text-white bg-red-600 px-4 py-2 text-sm font-medium shadow-sm">
+            Logout
+          </button>
         </div>
       </div>
     </div>
