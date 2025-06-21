526df43cd2575e4d02a30656e10cd266f7041eaf
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-06-21 20:27:54 +0530

    getUserDetails is done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,15 @@ import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React , {useState} from "react";
+import { link } from "fs";
 
 export default function ProfilePage() {
+
   const router = useRouter();
+
+  const [data, setData] = React.useState("nothing");
+
   const logout = async () => {
     // Implement your logout logic here
     try {
@@ -18,16 +23,31 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    try {
+      const response = await axios.get("/api/users/me");
+      // Handle user data as needed
+      console.log("User data:", response.data);
+      setData(response.data.data._id);
+      toast.success("User details fetched successfully");
+    } catch (error: any) {
+      console.error("Failed to fetch user details:", error.message);
+      toast.error(error.message);
+    }
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-black py-12 px-4 sm:px-6 lg:px-8">
       <div className="card max-w-md w-full space-y-8">
         <div>
-          <h2 className="text-center text-3xl font-extrabold">
+          <h1 className="text-center text-3xl font-extrabold">
             Your Profile
-          </h2>
-          <p className="mt-4 text-center text-gray-300">
-            Manage your account settings and preferences here.
-          </p>
+          </h1>
+           <h2 className="text-center text-2xl font-bold">{data === "nothing" ? "Nothing" : 
+            <Link href={`/profile/${data}`}> 
+              {data}
+            </Link>}
+            </h2>
         </div>
         
         <div className="mt-8 space-y-6">
@@ -54,6 +74,13 @@ export default function ProfilePage() {
             </button>
           </div>
           <hr />
+          
+          <button
+            onClick={getUserDetails}
+            className= "btn-primary w-full hover:bg-blue-700 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 rounded-md text-white bg-blue-600 px-4 py-2 text-sm font-medium shadow-sm">
+            Get User Details
+          </button>
+
           <button 
           onClick={logout}
           className="btn-danger w-full mt-4 hover:bg-red-700 transition-colors duration-200  focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 rounded-md text-white bg-red-600 px-4 py-2 text-sm font-medium shadow-sm">
